7dc0fe5105c41fb84dfff2bc9e30e7ec3e95c053
Gabriel Dezena <gabriel.deztor@gmail.com>
2021-11-30 20:31:05 -0300

    Add another exercicios file

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/script.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/script.js
@@ -28,21 +28,58 @@
 
 
 //exercicio 3
-let n = 5;
+// let n = 5;   //tamanho base e altura
+// let line = '';
+// let simbolo = '*';
+// let posicao = n;
+
+
+// //Loop para imprimir as linhas seguintes
+// for (index = 0; index < n; index +=1){
+
+
+//     //Loop para imprimir a primeira linha 
+//     for(let coluna =0; coluna <= n; coluna +=1){
+//         if(coluna<posicao){
+//                 line = line + ' ';
+//         }else {
+//             line = line + simbolo;
+//         }
+//     }
+//     console.log(line)  //imprime a linha em questão
+//     line = '';    // Zera o elemento line
+//     posicao -= 1; // Subtração para que a linha seguinte consiga aumentar o número de asteriscos
+
+// }
+
+
+//exercicio4
+
+let n = 5; //tamanho base
 let line = '';
 let simbolo = '*';
-let posicao = n;
+let meioPiramide = (n+1)/2;
+//Precisa de duas variaveis para controlar os dois lados da pirâmide
+let controleEsquerda = meioPiramide;
+let controleDireita = meioPiramide;
 
-for (index = 0; index < n; index +=1){
 
+// meio da piramide é a posição do centro da piramide
+for (let index =0; index < meioPiramide; index += 1){
+
+    //Loop para imprimir a primeira linha 
     for(let coluna =0; coluna <= n; coluna +=1){
-        if(coluna<posicao){
+
+        //A condição if abaixo é para construir os asteriscos do lado esquerdo e direito baseado no range deles.
+            if(coluna >= controleEsquerda && coluna <= controleDireita){
+                    line = line + simbolo;
+            }else {
                 line = line + ' ';
-        }else {
-            line = line + simbolo;
+                }
+            }
+        console.log(line)  //imprime a linha em questão
+        line = '';    // Zera o elemento line
+        controleEsquerda -= 1; // Subtração para que a linha seguinte consiga aumentar o número de asteriscos
+        controleDireita += 1; // Adição para que a linha seguinte consiga aumentar o número de asteriscos
+
         }
-    }
-    console.log(line)
-    line = '';
-    posicao -= 1;
-}

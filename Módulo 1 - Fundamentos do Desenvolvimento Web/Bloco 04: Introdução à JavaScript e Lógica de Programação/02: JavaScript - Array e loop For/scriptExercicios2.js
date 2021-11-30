e07222e32f73163b8f79ccae2b0b3db4fc0c8b13
Gabriel Dezena <gabriel.deztor@gmail.com>
2021-11-30 18:13:08 -0300

    Add another exercicios file

---
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/scriptExercicios2.js
@@ -0,0 +1,87 @@
+//Exercicio 1
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let numeros of numbers){
+//     console.log(numeros);
+// }
+
+// for (index = 0; index<numbers.length; index +=1){
+//     console.log(numbers[index]);
+// }
+
+//Exercicio 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+
+// for (let index = 0; index<numbers.length; index +=1){
+//     sum += numbers[index];
+// }
+// console.log (sum);
+
+//Exercício 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum =0;
+// let average;
+
+// for(let index = 0; index < numbers.length; index +=1){
+//     sum += numbers[index]
+// }
+//     average = sum/numbers.length;
+//     console.log (average);
+
+//Exercicio4
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum =0;
+// let average;
+
+// for(let index = 0; index < numbers.length; index +=1){
+//     sum += numbers[index]
+// }
+// average = sum/numbers.length;
+//     if(average >20){
+//         console.log('Valor maior que 20');
+//     } else {
+//         console.log ('valor menor ou igual a 20');
+//     }
+
+
+//Exercicio 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// maiorValor = 0;
+
+// for (let index = 0; index<numbers.length; index +=1){
+//     if(numbers[index]>maiorValor){
+//         maiorValor = numbers[index];
+//     }
+// }
+// console.log(maiorValor);
+
+
+// for (let x of numbers){
+//     if(x>maiorValor){
+//         maiorValor = x
+//     }
+// }
+// console.log(maiorValor);
+
+
+//Exercicio6
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let count=0;
+
+// for (let index = 0; index<numbers.length; index +=1){
+//     if(numbers[index]%2===1){
+//         count += 1;
+//     }
+// }
+// if(count ===0){
+//     console.log('nenhum valor ímpar encontrado');
+// } else{
+//     console.log(count);
+// }
+
+//exercicio7
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+

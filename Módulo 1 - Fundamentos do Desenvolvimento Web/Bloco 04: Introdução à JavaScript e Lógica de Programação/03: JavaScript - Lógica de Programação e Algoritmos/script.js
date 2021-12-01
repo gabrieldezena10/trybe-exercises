0e4ec9f19abe67cce6602c4a9d48fb7deb8a61ee
Gabriel Dezena <gabriel.deztor@gmail.com>
2021-12-01 10:55:44 -0300

    Update Files

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/script.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/script.js
@@ -95,30 +95,48 @@
 //5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
 
 
-let n = 7;
-let middle = (n + 1) / 2;
-let controlLeft = middle;
-let controlRight = middle;
-let symbol = '*';
+// let n = 7;
+// let middle = (n + 1) / 2;
+// let controlLeft = middle;
+// let controlRight = middle;
+// let symbol = '*';
 
 
-for (let line = 1; line <= middle; line += 1) {
-    let outputLine = '';
+// for (let line = 1; line <= middle; line += 1) {
+//     let outputLine = '';
 
-  for (let coluna = 1; coluna <= n; coluna += 1) {
-    if (coluna == controlLeft || coluna == controlRight || line == middle) {
-      outputLine += symbol;
-    } else {
-      outputLine += ' ';
-    }
-  }
-  controlLeft -= 1;
-  controlRight += 1;
-  console.log(outputLine);
-}
+//   for (let coluna = 1; coluna <= n; coluna += 1) {
+//     if (coluna == controlLeft || coluna == controlRight || line == middle) {
+//       outputLine += symbol;
+//     } else {
+//       outputLine += ' ';
+//     }
+//   }
+//   controlLeft -= 1;
+//   controlRight += 1;
+//   console.log(outputLine);
+// }
 
 
 //Exercício 6 - Bônus
 /*Faça um programa que diz se um número definido numa variável é primo ou não.
 Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
 Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.*/
+
+let n = 73;
+let numeroDivisores = 0
+let array = [];
+
+for (let index = 1; index<=n; index+=1){
+  if (n % index ===0){
+    numeroDivisores +=1;
+    array.push(index);
+  }
+}
+console.log(array);
+
+if (numeroDivisores ===2){
+  console.log("O número", n ,"é primo");
+} else {
+  console.log("O número", n , "não é primo");
+}

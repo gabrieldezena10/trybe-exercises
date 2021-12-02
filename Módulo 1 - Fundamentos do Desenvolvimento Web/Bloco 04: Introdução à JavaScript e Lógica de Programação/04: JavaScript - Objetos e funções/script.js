55c838dd1f73c667ebf04f33556c26e14f203963
Gabriel Dezena <gabriel.deztor@gmail.com>
2021-12-02 09:27:24 -0300

    Add scriptExercicios.js file

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/04: JavaScript - Objetos e funções/script.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/04: JavaScript - Objetos e funções/script.js
@@ -1,4 +1,4 @@
-/*
+/* OBJETOS
 1 - Crie um objeto player contendo as variáveis listadas abaixo.
 Copiar
 let name = 'Marta';
@@ -120,15 +120,15 @@ Agora veremos uma variação do laço for , que nos garante facilidade ao lidar 
 
 //Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
-function angulos(a,b,c){
-    if(a > 0 && b > 0 && c > 0){
-        if (a + b + c === 180){
-            return ' Os ângulos dos triângulos são: '+ a + '°, '+b+ '° e '+ c+'°.'
-        } else{
-            return 'Ângulos inválidos'
-        }
-}else{
-    return('Erro, ângulo negativo')
-}
-}
-console.log(angulos(30,30,120));
+// function angulos(a,b,c){
+//     if(a > 0 && b > 0 && c > 0){
+//         if (a + b + c === 180){
+//             return ' Os ângulos dos triângulos são: '+ a + '°, '+b+ '° e '+ c+'°.'
+//         } else{
+//             return 'Ângulos inválidos'
+//         }
+// }else{
+//     return('Erro, ângulo negativo')
+// }
+// }
+// console.log(angulos(30,30,120));

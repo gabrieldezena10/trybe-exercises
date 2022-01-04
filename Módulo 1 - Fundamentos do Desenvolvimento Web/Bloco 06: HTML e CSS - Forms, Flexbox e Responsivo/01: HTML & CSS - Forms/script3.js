fcfe40603ab1b5c3a3191b4fede88820a5cee6e7
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-01-04 13:57:13 -0300

    Update exercicios

---
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 06: HTML e CSS - Forms, Flexbox e Responsivo/01: HTML & CSS - Forms/script3.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('#buttonSubmit');
+let inputNameUser = document.querySelector('#inputNameUser');
+const nameUser = document.querySelector('#nameUser');
+
+function getUserValue(event) {
+  event.preventDefault();
+  console.log(inputNameUser.value)
+  nameUser.innerHTML = inputNameUser.value
+  inputNameUser.value=''
+}
+
+button.addEventListener('click', getUserValue);
